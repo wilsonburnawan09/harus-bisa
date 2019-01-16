268fa6a2132afe36f9712cb2d14c211b1ee248bd
Wilson Burnawan <wilson.burnawan@gmail.com>
2019-01-16 16:47:45 -0600

    add validation on authController.js

--- model/User.js
+++ model/User.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var userSchema = new mongoose.Schema({  
   first_name: String,
   last_name:String,
-  email: String,
+  email: { type: String, index: true},
   school: String,
   role: String,
   password: String,
