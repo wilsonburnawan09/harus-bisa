0c6a506e1a2488ccfedaeb84eed7f052792ae265
Wilson Burnawan <wilson.burnawan@gmail.com>
2019-01-15 22:36:42 -0600

    cancel changes on server.js

--- server.js
+++ server.js
@@ -1,18 +1,6 @@
 var app = require('./app');
-const http = require('http');
 var port = process.env.PORT || 3000;
-var host ='http://ec2-18-206-115-15.compute-1.amazonaws.com/';
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+var server = app.listen(port, function() {
+  console.log('Express server listening on port ' + port);
 });
-
-server.listen(port, host, () => {
-  console.log(`Server running at http://${host}:${port}/`);
-});
-
-// var server = app.listen(port, host, function() {
-//   console.log('Express server listening on port ' + port);
-// });
