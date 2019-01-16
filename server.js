a768ffe1fb1ddde6cf5f000e908948e4137d9473
Wilson Burnawan <wilson.burnawan@gmail.com>
2019-01-15 22:34:26 -0600

    try to fix bug at server.js

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ var app = require('./app');
 var port = process.env.PORT || 3000;
 var host ='http://ec2-18-206-115-15.compute-1.amazonaws.com/';
 
-var server = app.listen(port, host, function() {
-  console.log('Express server listening on port ' + port);
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
 });
+
+server.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}/`);
+});
+
+// var server = app.listen(port, host, function() {
+//   console.log('Express server listening on port ' + port);
+// });
