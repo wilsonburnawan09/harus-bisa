73024ca14d3ffabbb32c95bcf6d32483dfa6037e
Wilson Burnawan <wilson.burnawan@gmail.com>
2019-01-15 22:31:41 -0600

    fix bug at server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var app = require('./app');
 var port = process.env.PORT || 3000;
-var host ='http://ec2-18-206-115-15.compute-1.amazonaws.com';
+var host ='http://ec2-18-206-115-15.compute-1.amazonaws.com/';
 
 var server = app.listen(port, host, function() {
   console.log('Express server listening on port ' + port);
