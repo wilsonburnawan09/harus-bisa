7a27b5d3760701f1d33cc4061d2ec49a42478165
Wilson Burnawan <wilson.burnawan@gmail.com>
2019-01-16 20:05:08 -0600

    try to fix cors issue

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ var db = require('./db');
 
 app.use(cors());
 
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 var UserController = require('./controller/user/UserController');
 app.use('/users', UserController);
 
 var authController = require('./controller/auth/authController');
 app.use('/api/', authController);
 
-app.get('/', function (req, res) {
-    res.send("Hello")
-});
+// app.get('/', function (req, res) {
+//     res.send("Hello")
+// });
 module.exports = app;
