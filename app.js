b6b07855b860f1a68416bf01b9259c67364ffe32
Wilson Burnawan <wilson.burnawan@gmail.com>
2019-01-16 19:57:25 -0600

    add cors capability

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
 var db = require('./db');
+
+app.use(cors());
 
 var UserController = require('./controller/user/UserController');
 app.use('/users', UserController);
