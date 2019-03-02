c2c9a5f6e313d3e2ce0f75257213ef179518a84e
Wilson Burnawan <wilson.burnawan@gmail.com>
2019-03-02 10:53:50 -0600

    edit app.js to edit userController path

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.all('/', function(req, res, next) {
     next();
 });
 
-var UserController = require('./controlleruserController');
+var UserController = require('./controller/userController');
 app.use('/api/users', UserController);
 
 var authController = require('./controller/auth/authController');
