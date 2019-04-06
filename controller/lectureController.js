b65e77e93a9f88ed1f4409d764b8aeb7e5ceb788
Wilson Burnawan <wilson.burnawan@gmail.com>
2019-04-06 11:15:29 -0500

    debugging

--- controller/lectureController.js
+++ controller/lectureController.js
@@ -93,8 +93,7 @@ router.put('/:lecture_id', verifyToken, function(req,res,next){
                 if (req.body.date) { course.lectures[i].date = req.body.date.trim(); }
                 if (req.body.description) { 
                     if (req.body.description.trim() == ""){
-                        course.lectures[i].description = "";
-                        console.log('whitespace');
+                        course.lectures[i].description = " ";
                     } else {
                         course.lectures[i].description = req.body.description.trim(); 
                     }
