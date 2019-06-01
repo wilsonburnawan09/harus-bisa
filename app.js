8e2906405b532d0dce2a18a239d601a3ffe4e570
Wilson Burnawan <wilson.burnawan@gmail.com>
2019-06-01 14:45:59 -0500

    student will listen to all lecture in course

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ io.on("connection", socket => {
                 console.log('The user is in room: ', socket.rooms);
             });
         } else {
-            console.log('The user is a ', socket.role);
+            console.log('The user is a ', socket.user_role);
             console.log('The user is in room: ', socket.rooms);
         }
         socket.emit("saved_credential", { message: "User info is stored."})
