fb33018cf49bc3ffd65c0645fa324ad1f495789e
Wilson Burnawan <wilson.burnawan@gmail.com>
2019-06-04 17:07:39 -0500

    debugging participate_lecture

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ io.on("connection", socket => {
     socket.on("participate_lecture", (data) => {
         var room = data.course_id + "-" + data.lecture_id;
         if (socket.valid_rooms.includes(room)) {
-            io.in(room).emit("new_student_join", socket.user_id);
+            io.in(room).emit("new_student_join", socket.user_id).then(()=> console.log('hey'));
             console.log("fired");
             console.log(room);
         }
