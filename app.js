8dca610b53d7fceb0e259d36e1be786ad27b0217
Wilson Burnawan <wilson.burnawan@gmail.com>
2019-06-03 20:20:42 -0500

    give toggle is live notif to student when live

--- app.js
+++ app.js
@@ -47,29 +47,32 @@ io.on("connection", socket => {
         socket.user_id = data.user_id;
         socket.user_role = data.user_role;
         socket.valid_rooms = [];
-        // socket.lecture_ids = data.lecture_ids;
         data.lecture_ids.forEach(lecture => {
-            var room = data.course_id + lecture;
+            var room = data.course_id + "-" + lecture;
             socket.valid_rooms.push(room);
             if (socket.user_role === "student") {
                 socket.join(room);
+            } else if (socket.user_role === "professor") {
+                socket.live_lectures = new Set();
             }
         });
-        console.log('The user is a ', socket.user_role);
-        console.log('The user is in room: ', Object.keys( io.sockets.adapter.sids[socket.id] ));
     });
+    socket.on("get_info", ()=> {
+        console.log('The user id is: ', socket.user_id);
+        console.log('The user role is: ', socket.user_role);
+        console.log('The user is in room: ', Object.keys( io.sockets.adapter.sids[socket.id] ));
+    })
 
     socket.on("toggle_lecture_live", (data) => {
-        console.log('hey')
-        var room = data.course_id + data.lecture_id;
+        var room = data.course_id + "-" + data.lecture_id;
         if (socket.user_role === "professor" && socket.valid_rooms.includes(room)){
             var live = null;
             Course.findById(data.course_id, function(err, course){
-                if (course.instructor_id == data.user_id) {
+                if (course.instructor_id == socket.user_id) {
                     for(var i=0; i<course.lectures.length; i++){
                         if ( course.lectures[i].id == data.lecture_id) {
                             course.lectures[i].live = !course.lectures[i].live;
-                            live = course.lectures[i].live
+                            live = course.lectures[i].live;
                             break;
                         }
                     }
@@ -78,19 +81,13 @@ io.on("connection", socket => {
                         if (live) {
                             socket.join(room, () => { 
                                 socket.to(room).emit("lecture_is_live", true);
-                                console.log('The user is in room: ', Object.keys( io.sockets.adapter.sids[socket.id] ));
+                                // console.log('The user is in room: ', Object.keys( io.sockets.adapter.sids[socket.id] ));
+                                socket.live_lectures.add(room);
                             });
-                        } else {
-                            socket.to(room).emit("lecture_is_live", false);
-                            io.in(room).clients(async function(error, clients) {
-                                if (clients.length > 0) {
-                                    console.log('clients in the room: ');
-                                    console.log(clients);
-                                    await clients.forEach(function (socket_id) {
-                                    io.sockets.sockets[socket_id].leave(room);
-                                    });
-                                    console.log('The user is in room: ', Object.keys( io.sockets.adapter.sids[socket.id] ));
-                                }
+                        } else if(!live) {
+                            socket.leave(room, () => {
+                                socket.to(room).emit("lecture_is_live", false);
+                                socket.live_lectures.delete(room);
                             });
                         }
                     });
@@ -99,9 +96,46 @@ io.on("connection", socket => {
         }
     });
 
+    socket.on("participate_lecture", (data) => {
+        var room = data.course_id + "-" + data.lecture_id;
+    });
+
 
     socket.on("disconnect", () => {
         console.log("user ", socket.user_id, " ", socket.user_role);
+        if (socket.user_role === "professor") {
+            var connected_lectures = socket.live_lectures.entries();
+            for (var room of connected_lectures){
+                var cleaned_room = room[0];
+                var split_pos = 0;
+                for( var i=0; i<cleaned_room.length; i++){
+                    if (cleaned_room.charAt(i) == '-') {
+                        split_pos = i;
+                        break;
+                    }
+                }
+                var course_id = cleaned_room.slice(0,i);
+                var lecture_id = cleaned_room.slice(i+1);
+                // console.log(course_id);
+                // console.log(lecture_id);
+                Course.findById(course_id, function(err, course){
+                    if (course.instructor_id == socket.user_id) {
+                        for(var i=0; i<course.lectures.length; i++){
+                            if ( course.lectures[i].id == lecture_id) {
+                                course.lectures[i].live = false;
+                                break;
+                            }
+                        }
+                        course.markModified('lectures');
+                        course.save().then( () => {
+                            console.log('emit?')
+                            io.in(room).emit("lecture_is_live", false);
+                        });
+                    }
+                });
+            }
+            
+        }
     });
 });
 
