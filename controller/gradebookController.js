58edc4514d5420c3ca3587282f90c40b996ad234
Wilson Burnawan <wilson.burnawan@gmail.com>
2019-07-27 21:14:44 -0700

    give quiz_id and quiz_number for quiz gradebook api

--- controller/gradebookController.js
+++ controller/gradebookController.js
@@ -126,6 +126,70 @@ router.get('/professor/courses/:course_id/lectures/:lecture_id/quizzes', verifyT
         }
         if (lecture_info == null || lecture_info.has_lived == false) return res.status(404).send({ message: "Lecture " + req.params.lecture_id + " not found or never started.", data: null });
         var participation_reward = lecture_info.participation_reward_percentage;
+        var quiz_number = 1;
+        lecture_info.quizzes.forEach(quiz => {
+            var total_participants = 0;
+            var average_score = 0;
+            var accuracy_pts = 0;
+            var participation_pts = 0;
+            var max_accuracy_pts = 0;
+            for (var [user_id, course_answers] of course.course_gradebook.entries()){
+                if (course_answers.role != "professor") {
+                    if (course_answers.lecture_grades[req.params.lecture_id].present && course_answers.lecture_grades[req.params.lecture_id].quiz_answers[quiz.id] != undefined) {
+                        if (course_answers.lecture_grades[req.params.lecture_id].quiz_answers[quiz.id] == quiz.correct_answer) {
+                            accuracy_pts += ( (100 - participation_reward) / 100 ) * quiz.point;
+                        }
+                        total_participants += 1;
+                        participation_pts += (participation_reward/100) * quiz.point;
+                        max_accuracy_pts += ( (100 - participation_reward) / 100 ) * quiz.point;
+                    }
+                }
+            }
+            average_score = ((accuracy_pts+participation_pts)/(max_accuracy_pts+participation_pts))*100
+            var quiz_gradebook = {
+                "quiz_numnber": quiz_number,
+                "quiz_id": quiz.id,
+                "question": quiz.question,
+                "include": quiz.include,
+                "total_participants": quiz.include ? total_participants.toString() : '-',
+                "average_score": quiz.include ? average_score.toFixed(2) : '-',
+            }
+            lecture_gradebooks.gradebooks.push(quiz_gradebook);
+            quiz_number += 1;
+        });
+        return res.status(200).send(lecture_gradebooks);
+    });
+});
+
+router.put('/professor/courses/:course_id/lectures/:lecture_id/quizzes/', verifyToken, async function(req,res,next){
+    if (req.role != "professor") return res.status(401).send({ message: "Only professors are allowed to see this page.", data: null});
+    Course.findById(req.params.course_id, async function(err, course){
+        if (err) { return res.status(500).send({ message: "There was a problem looking for the course.", data: null }); }
+        if (!course) return res.status(404).send({ message: "Course " + req.params.course_id + " not found.", data: null });
+        if (course.instructor_id != req.userId) return res.status(401).send({ message: "You are not the professor of this course.", data: null});
+
+        var lecture_info = null;
+        for (var i=0; i<course.lectures.length; i++){
+            if (course.lectures[i].id == req.params.lecture_id) {
+                lecture_info = course.lectures[i];
+                break;
+            }
+        }
+        if (lecture_info == null) return res.status(404).send({ message: "Lecture " + req.params.lecture_id + " not found.", data: null });
+
+        var lecture_gradebooks = {}
+        lecture_gradebooks["number_of_students"] = course.number_of_students;
+        lecture_gradebooks["class_average"] = await get_class_average(course)
+        lecture_gradebooks["gradebooks"] = []
+        var lecture_info = null;
+        for (var i=0; i<course.lectures.length; i++){
+            if (course.lectures[i].id == req.params.lecture_id) {
+                lecture_info = course.lectures[i];
+                break;
+            }
+        }
+        if (lecture_info == null || lecture_info.has_lived == false) return res.status(404).send({ message: "Lecture " + req.params.lecture_id + " not found or never started.", data: null });
+        var participation_reward = lecture_info.participation_reward_percentage;
         lecture_info.quizzes.forEach(quiz => {
             var total_participants = 0;
             var average_score = 0;
