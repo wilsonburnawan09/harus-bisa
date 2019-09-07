4b79ac6b33fbb7d171435395bf28e9299fe543ad
wilsonb2 <wilsonb2@illinois.edu>
2019-09-06 23:33:30 -0500

    change response message to be Indonesian in verifyTokenMiddleware.js

--- controller/auth/verifyTokenMiddleware.js
+++ controller/auth/verifyTokenMiddleware.js
@@ -5,9 +5,11 @@ function verifyToken(req, res, next) {
 		return res.status(403).send({ message: 'No authorization header provided.'});
 	}
   	var token = req.headers['authorization'].split(" ")[1];
-	if (!token) return res.status(403).send({ auth: false, message: 'No token provided.' });
+	// if (!token) return res.status(403).send({ auth: false, message: 'No token provided.' });
+	if (!token) return res.status(403).send({ auth: false, message: 'Token tidak diberikan.' });
   	jwt.verify(token, config.secret, function(err, decoded) {
-    	if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+		// if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+		if (err) return res.status(500).send({ auth: false, message: 'Terjadi kesalahan dalam autentikasi token.' });
 		req.userId = decoded.id;
 		req.first_name = decoded.first_name;
 		req.last_name = decoded.last_name;
