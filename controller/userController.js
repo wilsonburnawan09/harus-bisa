029a4f27ec5d1eec73ff45b62ff8f71c1d1b313c
wilsonb2 <wilsonb2@illinois.edu>
2019-09-20 20:54:18 -0500

    make sure new password is provided

--- controller/userController.js
+++ controller/userController.js
@@ -66,6 +66,7 @@ router.put('/:user_id', verifyToken, async function (req, res, next) {
         // }
 
         if (req.body.new_password != null) {
+            console.log("hello" + req.body.new_password + "world");
             if (req.body.new_password != "" ) return res.status(500).send({ message: "Mohon memberi kata sandi baru.", data: null});
             if (req.body.old_password == null) {
                 // return res.status(500).send({ message: "Please provide old password", data: null});
